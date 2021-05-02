bfef450835cc6b549ee80dbd89bf415ff806a9e7
Ivan_Velichko <showman.velichko@gmail.com>
2021-05-02 19:20:42 +0300

    save for continue

--- src/main/java/com/epam/secondtask/Main.java
+++ src/main/java/com/epam/secondtask/Main.java
@@ -1,99 +1,178 @@
 package com.epam.secondtask;
 
+import com.epam.secondtask.model.Medicine;
+import com.epam.secondtask.model.Version;
+import com.epam.secondtask.model.enumeration.MedicineGroupType;
+import com.epam.secondtask.model.enumeration.MedicinePackageType;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private final static String pathToFile = "src/main/resources/data/medicines.xml";
+
     public static void main(String[] args) {
-//        String pathToFile = "src/main/resources/data/medicines.xml";
+
+        List<Medicine> medicines = new ArrayList<>();
+        Document doc;
+        try {
+            doc = buildDocument();
+        } catch (Exception e) {
+            System.out.println("Open parsing error " + e.toString());
+            return;
+        }
+
+        Node rootNodeMedicines = doc.getFirstChild();  //получил корневой medicines
+        System.out.println("Самый корневой элемент - " + rootNodeMedicines.getNodeName());
+
+        NodeList medicinesChilds = rootNodeMedicines.getChildNodes(); //Получаю все обьекты Medicine, Vaccine и Homeopathy
+        Node medicineNode = null;
+        Node homeopathyNode = null;
+        Node vaccineNode = null;
+        int count = 0;
+        for (int i = 0; i < medicinesChilds.getLength(); i++) {
+            Medicine medicine = new Medicine();
+            if (medicinesChilds.item(i).getNodeType() != Node.ELEMENT_NODE) {  //фильтрую барахло типа #text
+                count += 1;
+                continue;
+            }
+            System.out.println("Ребенок Медицинс -  " + count + " " + medicinesChilds.item(i).getNodeName());
+
+            switch (medicinesChilds.item(i).getNodeName()) {
+                case "medicine" -> {
+                    medicineNode = medicinesChilds.item(i);
+                    buildMedicine(medicine, medicineNode);
+                }
+                case "homeopathy" -> {
+                    homeopathyNode = medicinesChilds.item(i);
+                    buildHomeopathy();
+                }
+                case "vaccine" -> {
+                    vaccineNode = medicinesChilds.item(i);
+                    buildVaccine();
+                }
+            }
+            medicines.add(medicine);
+            }
+    }
+
+    private static void buildVaccine() {
+    }
+
+    private static void buildHomeopathy() {
+    }
+
+    private static void buildMedicine(Medicine medicine, Node node) {
+        if (node == null) {
+            return;
+        }
+        NodeList medicineChilds = node.getChildNodes();
+        for (int j = 0; j < medicineChilds.getLength(); j++) {
+            Node nextMedicineChild = medicineChilds.item(j);
+            if (nextMedicineChild.getNodeType() != Node.ELEMENT_NODE) {
+                continue;
+            }
+//                        String medicineID = getTextContentByElement(node, "id");   //todo
+//            medicine.setMedicineId(medicineID);//todo
+//            boolean isMedicinePrescription = Boolean.parseBoolean(getTextContentByElement(node, "prescription"));//todo
+//            medicine.setPrescription(isMedicinePrescription);//todo
+            String medicineGroup = getTextContentByElement(node, "group");
+            medicine.setMedicineGroup(MedicineGroupType.valueOf(medicineGroup.toUpperCase()));
+            String medicineName  = getTextContentByElement(node, "name");
+            medicine.setMedicineName(medicineName);
+            System.out.println("Ребенок Медицины - " + nextMedicineChild.getNodeName());
+            switch (nextMedicineChild.getNodeName()) {
+                case "analogs" ->
+                        buildMedicineAnalogsList(medicine, nextMedicineChild);
+                case "versions"->
+                    buildMedicineVersionsList(medicine, nextMedicineChild);
+            }
+        }
+    }
+
+    private static void buildMedicineVersionsList(Medicine medicine, Node nodeVersions) {
+        List<Version> versions = new ArrayList<>();
+        NodeList childNodes = nodeVersions.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node item = childNodes.item(i);
+            if (item.getNodeType() == Node.ELEMENT_NODE) {
+                Version version = readVersion(item);
+
+                versions.add(version);
+            }
+
+        }
+        medicine.setMedicineVersions(versions);
+    }
+
+
+    private static void buildMedicineAnalogsList(Medicine medicine, Node nodeAnalogs) {
+        List<String> analogs = new ArrayList<>();
+        NodeList childNodes = nodeAnalogs.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node item = childNodes.item(i);
+            if (item.getNodeType() == Node.ELEMENT_NODE) {
+                String analogContent = item.getTextContent();
+                analogs.add(analogContent);
+            }
+
+        }
+        medicine.setAnalogs(analogs);
+    }
+
+    private static Version readVersion(Node nodeVersion) {
+        Version version = new Version();
+        String packageTextContent = getTextContentByElement(nodeVersion, "package");
+        version.setMedicinePackage(MedicinePackageType.valueOf(packageTextContent.toUpperCase()));
+        String pharmCompany = getTextContentByElement(nodeVersion, "pharm");
+        version.setPharmCompany(pharmCompany);
+        String dosageMedicine = getTextContentByElement(nodeVersion, "dosage");
+        version.setMedicineDosage(dosageMedicine);
+        String certificateMedicine = getTextContentByElement(nodeVersion, "certificate");
+        version.setMedicineCertificate(certificateMedicine);
+        String expirationDate = getTextContentByElement(nodeVersion,"expiration_date");
+        version.setExpirationDate(YearMonth.parse(expirationDate));
+        return version;
+    }
+
+    private static String getTextContentByElement(Node node, String childElementName) {
+        Element versionElement = (Element) node;
+        NodeList elementsByTagName = versionElement.getElementsByTagName(childElementName);
+        Node item = elementsByTagName.item(0);
+        return item.getTextContent();
+    }
+
+    private static Document buildDocument() throws Exception {
+        File file = new File(pathToFile);
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        return builderFactory.newDocumentBuilder().parse(file);
 
     }
 }
 
-
-
-
-
-
-
-
-
-//    Medicine medicine = null;
-//    File file = new File("src/main/resources/data/medicines.xml");
-//
-//    DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-//    Document document = null;
-//        try {
-//                document = builderFactory.newDocumentBuilder().parse(file);
-//                } catch (Exception e) {
-//                System.out.println("Open parsing error " + e.toString());
-//                return;
+//                if (analogNode != null) {
+//                    NodeList analogs = analogNode.getChildNodes();
+//                    for (int j = 0; j < analogs.getLength(); j++) {
+//                        if (analogs.item(j).getNodeType() != Node.ELEMENT_NODE) {
+//                            continue;
+//                        }
+//                        System.out.println("Ребенок Аналогов - " + analogs.item(j).getNodeName());
+//                    }
 //                }
 //
-//                Node rootNode = document.getFirstChild();  //получил корневой medcines
-//                System.out.println(rootNode.getNodeName());
-//
-//                NodeList childNodes = rootNode.getChildNodes();
-
-
-//далее три важных обьекта в DOM - Node/ NodeList / Element
-//Node это абсолютно любой элемент файла XML - строка, пробел, элемент и тд. Любой пишк это обьект нод
-//Nodelist - например просим лист всех дочерних элемнтов рута и тд
-//Element - этоуже конкретный обьект, тег
-
-//        Node rootNode = doc.getFirstChild();                                    1 способ достать элементы
-//        System.out.println("Корневой нод (элемент) " + rootNode.getNodeName());
-//        NodeList childNodes = rootNode.getChildNodes();
-//        Root root = new Root();
-//        Document doc;
-//        try {
-//            doc = buildDocument();
-//        } catch (Exception e) {
-//            System.out.println("Open parsing error" + e.toString());
-//            return;
-//        }
-//
-//        Node rootNode = doc.getFirstChild();
-//
-//        NodeList rootChilds = rootNode.getChildNodes();
-//
-//        String mainNAme = null;
-//        Node peopleNode = null;
-//        for (int i = 0; i < rootChilds.getLength(); i++) {
-//            if (rootChilds.item(i).getNodeType() != Node.ELEMENT_NODE) { //проверка только на элементы, а не на все что видит нод (пустые строки, отсутпы и тд)
-//                continue;
-//            }
-//            System.out.println("Print " + rootChilds.item(i).getNodeName());
-//
-//            switch (rootChilds.item(i).getNodeName()) {
-//                case "name": {
-//                    mainNAme = rootChilds.item(i).getTextContent();
-//                    System.out.println("Main name " + mainNAme); // печатаем содержимое main name
-//
+//                if (versionNode != null) {
+//                    NodeList versions = versionNode.getChildNodes();
+//                    for (int j = 0; j < versions.getLength(); j++) {
+//                        if (versions.item(j).getNodeType() != Node.ELEMENT_NODE) {
+//                            continue;
+//                        }
+//                        System.out.println("Ребенок Версий - " + versions.item(j).getNodeName());
+//                    }
 //                }
-//                case "people": {
-//                    peopleNode = rootChilds.item(i);
-//                }
-//            }
-//        }
-//        root.setName(mainNAme);
-//
-//        if (peopleNode == null) {
-//            return;
-//        }
-//        List<People> peopleList = new ArrayList<>();
-//        NodeList peopleChilds = peopleNode.getChildNodes();
-//        for (int i = 0; i < peopleChilds.getLength(); i++) {
-//            if (peopleChilds.item(i).getNodeType() != Node.ELEMENT_NODE) { //проверка только на элементы, а не на все что видит нод (пустые строки, отсутпы и тд)
-//                continue;
-//            }
-//            if (!peopleChilds.item(i).getNodeName().equals("element")) {
-//                continue;
-//            }
-//
-//        }
-//
-//    }
-//
-//    private static Document buildDocument() throws Exception {
-//        File file = new File("src/main/resources/data/test.xml");
-//        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-//        return dbf.newDocumentBuilder().parse(file);
-//    }  //ксок кода от обьявления файла, открывает файл для парсинга
--- src/main/java/com/epam/secondtask/builder/MedicinesDomBuilder.java
+++ src/main/java/com/epam/secondtask/builder/MedicinesDomBuilder.java
@@ -2,8 +2,10 @@ package com.epam.secondtask.builder;
 
 import com.epam.secondtask.model.Medicine;
 import com.epam.secondtask.model.Version;
+import com.epam.secondtask.model.enumeration.MedicineGroupType;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -11,8 +13,8 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class MedicinesDomBuilder {
@@ -29,9 +31,11 @@ public class MedicinesDomBuilder {
             e.printStackTrace(); // log
         }
     }
+
     public Set<Medicine> getMedicines() {
         return medicines;
     }
+
     public void buildSetMedicines(String filename) {
         Document doc;
         try {
@@ -51,17 +55,21 @@ public class MedicinesDomBuilder {
 
     private Medicine buildMedicine(Element medicineElement) {
         Medicine medicine = new Medicine();
+        ArrayList<String> analogs = new ArrayList<>();
 // add null check
         medicine.setMedicineId((medicineElement.getAttribute("id")));
         medicine.setPrescription(Boolean.parseBoolean(medicineElement.getAttribute("prescription")));
         medicine.setMedicineName(getElementTextContent(medicineElement, "name"));
-        medicine.setMedicineGroup(getElementTextContent(medicineElement, "name"));
-        medicine.setAnalogs(getElementTextContent(medicineElement, "name"));
-        medicine.setMedicineVersions(getElementTextContent(medicineElement, "name"));
+        medicine.setMedicineGroup(MedicineGroupType.valueOf(getElementTextContent(medicineElement,"group").toUpperCase())); //todo
 
+        NodeList analogs1 = medicineElement.getChildNodes();
+        Node item = analogs1.item(0);
+        for (int i = 0; i < analogs1.getLength(); i++) {
+            Node analog = analogs1.item(i);
+            analog.getTextContent();
 
-
-        for (int i = 0; i <medicineElement.getElementsByTagName("versions").getLength(); i++) {
+        }
+        for (int i = 0; i < medicineElement.getElementsByTagName("versions").getLength(); i++) {
             Version version = new Version();
             Element versionElement = (Element) medicineElement.getElementsByTagName("version").item(i);
             version.setPharmCompany(getElementTextContent(versionElement, "pharm"));
@@ -69,14 +77,14 @@ public class MedicinesDomBuilder {
             version.setMedicineCertificate(getElementTextContent(versionElement, "certificate"));
             medicine.getMedicineVersions().add(version);
         }
-
-
-
-        versions.s(getElementTextContent(adressElement, "country"));
-        versions.setCity(getElementTextContent(adressElement, "city"));
-        versions.setStreet(getElementTextContent(adressElement, "street"));
-        medicine.setMedicineId(medicineElement.getAttribute("id"));
         return medicine;
+    }
+
+    private static String getElementTextContent(Element element, String elementName) {
+        NodeList nList = element.getElementsByTagName(elementName);
+        Node node = nList.item(0);
+        String text = node.getTextContent();
+        return text;
     }
 }
 
